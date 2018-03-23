c91fa1b12c6880684d2725a42d88618685e7cae8
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-23 14:46:19 +0000

    lesson 15

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ new Vue({
   el: '#app',
   data: {
     title: 'Becoming a Vue ninja',
-    showName: true,
-    showAge: true
+    items: ['Mushroom', 'Green Shells', 'Red Shells', 'Banana', 'Star'],
+    ninjas: [
+      { name: 'Crystal', age: 25, belt: 'Black' },
+      { name: 'Ryu', age: 30, belt: 'Brown' },
+      { name: 'Ken', age: 35,belt: 'Orange' }
+    ]
   },
   methods: {
-    toggleName(){
-      this.showName = !this.showName
-    },
-    toggleAge(){
-      this.showAge = !this.showAge
-    }
+
   }
 })

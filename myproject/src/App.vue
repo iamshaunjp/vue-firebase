763270309cbf9b27918923d879420c4c0e8cbe23
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-23 18:01:34 +0000

    lesson 19

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <Nav />
   </div>
 </template>
 
 <script>
+import Nav from './nav'
+
 export default {
   name: 'app',
+  components: {
+    Nav
+  },
   data () {
     return {
       title: 'My first Vue app, wooo :)'

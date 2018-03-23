9dd21cd8c16c638af70a83c6a22c2fb98f560caf
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-23 11:36:11 +0000

    lesson 9

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ new Vue({
   el: '#app',
   data: {
     title: 'Becoming a Vue ninja',
-    name: 'Ryu',
-    url: 'http://www.youtube.com',
-    classes: ['one', 'two']
+    wage: 10
   },
   methods: {
-    greet(time){
-      return `Hello and good ${time}, ${this.name}`
+    changeWage(amount){
+      this.wage += amount
     }
   }
 })

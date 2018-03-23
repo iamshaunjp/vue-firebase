766bdb5cf965e5325c2f34584d0f2642fa6c6119
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-23 11:45:32 +0000

    lesson 10

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ new Vue({
   el: '#app',
   data: {
     title: 'Becoming a Vue ninja',
-    wage: 10
+    coords: {
+      x: 0,
+      y: 0
+    }
   },
   methods: {
-    changeWage(amount){
-      this.wage += amount
+    logEvent(e){
+      console.log(e);
+    },
+    logCoords(e){
+      this.coords.x = e.offsetX
+      this.coords.y = e.offsetY
     }
   }
 })

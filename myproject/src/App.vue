bd2775d63d232ca0f440c07988601ed746d67703
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-24 13:21:04 +0000

    lesson 23

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,41 +2,27 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from './Navbar'
-import AllFriends from './AllFriends'
-import OnlineFriends from './OnlineFriends'
+import Blogs from './Blogs'
 
 export default {
   name: 'app',
   components: {
     Navbar,
-    AllFriends,
-    OnlineFriends
+    Blogs
   },
   data () {
     return {
-      title: 'My first Vue app, wooo :)',
-      friends: [
-        { name: 'Mario', online: true },
-        { name: 'Luigi', online: false },
-        { name: 'Toad', online: true },
-        { name: 'Bowser', online: false }
-      ]
+      title: 'My first Vue app, wooo :)'
     }
   },
-  methods: {
-    deleteFriend(payload){
-      console.log(payload)
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name
-      })
-    }
+  methods:{
+    
   }
 }
 </script>
---
+++ myproject/src/Blogs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="blogs">
+    <h2>{{ blogTitle }}</h2>
+    <button @click="changeTitle">Change title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blogs',
+  data(){
+    return {
+      blogTitle: 'Blogs'
+    }
+  },
+  methods: {
+    changeTitle(){
+      this.blogTitle = 'Amazing Blog Site'
+    }
+  },
+  beforeCreate(){
+    alert('beforeCreate hook')
+  },
+  created(){
+    alert('created hook')
+  },
+  beforeUpdate(){
+    alert('beforeUpdate hook')
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+

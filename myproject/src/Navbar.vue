10373f03d4ab9981bb439f77a6e59eed24c6d8d7
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-24 10:09:55 +0000

    lesson 20

---
+++ myproject/src/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav>
+    <h2>Menu</h2>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+nav{
+  text-align: center;
+}
+nav ul{
+  padding: 0;
+}
+nav li{
+  display: inline-block;
+  list-style-type: none;
+  margin: 10px;
+}
+</style>

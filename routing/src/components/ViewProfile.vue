2c8f5f6f4e580f213325032ad08dd329a8002ddf
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-25 10:07:21 +0100

    lesson 30 code

---
+++ routing/src/components/ViewProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="view-profile">
+    <h2>Profile for {{ userId }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewProfile',
+  data(){
+    return {
+      userId: this.$route.params.user_id
+    }
+  }
+}
+</script>
+
--- routing/src/router/index.js
+++ routing/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import About from '@/components/About'
+import ViewProfile from '@/components/ViewProfile'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'About',
       component: About
+    },
+    {
+      path: '/profile/:user_id',
+      name: 'ViewProfile',
+      component: ViewProfile
     }
   ]
 })

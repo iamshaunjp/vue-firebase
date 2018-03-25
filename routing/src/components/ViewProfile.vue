c07d8630b2c51c5075a4f992a09ffb97f5df69be
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-25 10:14:41 +0100

    lesson 31

--- routing/src/components/ViewProfile.vue
+++ routing/src/components/ViewProfile.vue
@@ -11,6 +11,14 @@ export default {
     return {
       userId: this.$route.params.user_id
     }
+  },
+  methods: {
+    updateId(){
+      this.userId = this.$route.params.user_id
+    }
+  },
+  watch: {
+    $route: 'updateId'
   }
 }
 </script>

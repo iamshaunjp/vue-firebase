64ec89e3758642f920ec5c00829fea1b059c3f11
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-25 09:07:29 +0100

    lesson 29

--- routing/src/App.vue
+++ routing/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+    <Navbar />
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
---
+++ routing/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+        <router-link :to="{ name: 'About' }">About</router-link>
+      </li>
+    </ul>  
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

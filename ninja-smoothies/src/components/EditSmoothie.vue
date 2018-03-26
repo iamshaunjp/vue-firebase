13141c8cef488c62e74315bad7211d766c0bcf62
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-26 15:50:13 +0100

    lesson 54

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -1,6 +1,25 @@
 <template>
-  <div v-if="smoothie" class="edit-smoothie container">
-    <h2>Edit {{ smoothie.title }} Smoothie</h2>
+  <div v-if="smoothie" class="edit-smoothie container z-depth-1">
+    <h2 class="indigo-text center-align">Edit {{ smoothie.title }} Smoothie</h2>
+    <form @submit.prevent="editSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie title:</label>
+        <input type="text" name="title" v-model="smoothie.title">
+      </div>
+      <div v-for="(ing, index) in smoothie.ingredients" class="field ingredient" :key="index">
+        <label for="ingredient">Ingredient:</label>
+        <input type="text" name="ingredient" v-model="smoothie.ingredients[index]">
+        <i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient (press tab to add):</label>
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+      </div>
+      <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
+        <button class="btn pink">Update Smoothie</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -11,14 +30,34 @@ export default {
   name: 'EditSmoothie',
   data(){
     return{
-      smoothie: null
+      smoothie: null,
+      another: null,
+      feedback: null
+    }
+  },
+  methods: {
+    editSmoothie(){
+
+    },
+    addIng(){
+      if(this.another){
+        this.smoothie.ingredients.push(this.another)
+        this.another = null
+        this.feedback = null
+      } else {
+        this.feedback = 'You must enter a value to add another ingredient'
+      }
+    },
+    deleteIng(ing){
+      this.smoothie.ingredients = this.smoothie.ingredients.filter(ingredient => {
+        return ingredient != ing
+      })
     }
   },
   created(){
     let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
     ref.get().then(snapshot => {
       snapshot.forEach(doc => {
-        console.log(doc.data())
         this.smoothie = doc.data()
         this.smoothie.id = doc.id
       })
@@ -28,6 +67,26 @@ export default {
 </script>
  
 <style>
- 
+.edit-smoothie{
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.edit-smoothie h2{
+  font-size: 2em;
+  margin: 20px auto;
+}
+.edit-smoothie .field{
+  margin: 20px auto;
+  position: relative;
+}
+.edit-smoothie .delete{
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
+}
 </style>
  

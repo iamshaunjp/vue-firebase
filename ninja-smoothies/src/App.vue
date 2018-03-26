6b96943a2bc69242442e4b2ca7247375a4a372e6
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-26 08:33:45 +0100

    lesson 39

--- ninja-smoothies/src/App.vue
+++ ninja-smoothies/src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ ninja-smoothies/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Ninja Smoothies </span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab pink">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.navbar nav{
+  padding: 0 20px;
+}
+</style>
+

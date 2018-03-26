522037dd4360f65c68cdf9a01b9a1f634614dffe
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-26 17:38:54 +0100

    lesson 61

---
+++ ninja-chat/src/components/Welcome.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name">
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data(){
+    return{
+      name: null
+    }
+  },
+  methods: {
+    enterChat(){
+      console.log(this.name)
+    }
+  }
+}
+</script>
+
+<style>
+.welcome{
+  max-width: 400px;
+  margin-top: 100px;
+}
+.welcome h2{
+  font-size: 3em;
+}
+.welcome button{
+  margin: 30px auto;
+}
+</style>
--- ninja-chat/src/router/index.js
+++ ninja-chat/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Welcome from '@/components/Welcome'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Welcome',
+      component: Welcome
     }
   ]
 })

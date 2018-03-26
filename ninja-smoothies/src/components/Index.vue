7109c033cef1b243a03dfda070ae74c890a3a3fe
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-26 09:50:01 +0100

    lesson 41

--- ninja-smoothies/src/components/Index.vue
+++ ninja-smoothies/src/components/Index.vue
@@ -2,6 +2,7 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
         <h2 class="indigo-text">{{ smoothie.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
@@ -23,6 +24,13 @@ export default {
         { title: 'Morning Mood', slug: 'morning-mood', ingredients: ['mango', 'lime', 'juice'], id: '2' }
       ]
     }
+  },
+  methods: {
+    deleteSmoothie(id){
+      this.smoothies = this.smoothies.filter(smoothie => {
+        return smoothie.id != id
+      })
+    }
   }
 }
 </script>
@@ -39,8 +47,16 @@ export default {
   text-align: center;
   margin-top: 0;
 }
-.ingredients{
+.index .ingredients{
   margin: 30px auto;
   display: flex;
+}
+.index .delete{
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
 }
 </style>

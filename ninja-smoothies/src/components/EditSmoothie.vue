6102449f1a08c72156759b3041a5f1bca6da5ae8
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-26 15:42:19 +0100

    lesson 53

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="edit-smoothie container">
-    <h2>Edit a Smoothie {{ this.$route.params.smoothie_slug }}</h2>
+  <div v-if="smoothie" class="edit-smoothie container">
+    <h2>Edit {{ smoothie.title }} Smoothie</h2>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
   name: 'EditSmoothie',
   data(){
     return{
-
+      smoothie: null
     }
+  },
+  created(){
+    let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+        console.log(doc.data())
+        this.smoothie = doc.data()
+        this.smoothie.id = doc.id
+      })
+    })
   }
 }
 </script>

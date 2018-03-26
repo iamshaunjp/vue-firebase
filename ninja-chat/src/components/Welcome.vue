fc508844a206a076fc5f1534167b614b0093c312
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-26 20:53:41 +0100

    lesson 63

--- ninja-chat/src/components/Welcome.vue
+++ ninja-chat/src/components/Welcome.vue
@@ -6,6 +6,7 @@
         <form @submit.prevent="enterChat">
           <label for="name">Enter your name:</label>
           <input type="text" name="name" v-model="name">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
           <button class="btn teal">Enter Chat</button>
         </form>
       </div>
@@ -19,12 +20,17 @@ export default {
   name: 'Welcome',
   data(){
     return{
-      name: null
+      name: null,
+      feedback: null
     }
   },
   methods: {
     enterChat(){
-      this.$router.push({ name: 'Chat', params: { name: this.name }})
+      if(this.name){
+        this.$router.push({ name: 'Chat', params: { name: this.name }})
+      } else {
+        this.feedback = "You must enter a name to join"
+      }
     }
   }
 }

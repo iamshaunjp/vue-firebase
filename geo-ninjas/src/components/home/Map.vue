9c4966339e98dfe90ce6c4c31b25bfa196492df5
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-27 17:12:43 +0100

    lesson-76

---
+++ geo-ninjas/src/components/home/Map.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="map">
+    <h2>Map</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
--- geo-ninjas/src/router/index.js
+++ geo-ninjas/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Map from '@/components/home/Map'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Map',
+      component: Map
     }
   ]
 })

ea13b2acd4c37c0c695f23bc602ad55e81832b7c
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-27 17:04:53 +0100

    lesson 75

--- geo-ninjas/src/App.vue
+++ geo-ninjas/src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/layout/Navbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="" class="brand-logo left">GeoNinjas!</a>
+        <ul class="right">
+          <li><a href="">Signup</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

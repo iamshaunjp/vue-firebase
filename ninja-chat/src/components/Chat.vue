b0c3f62875d9b619ea2711ed50d515ad9474ec55
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-27 12:40:28 +0100

    lesson 64

--- ninja-chat/src/components/Chat.vue
+++ ninja-chat/src/components/Chat.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="chat container">
-    <h2>Chat {{ this.name }}</h2>
+    <h2 class="center teal-text">Ninja Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">name</span>
+            <span class="grey-text text-darken-3">message</span>
+            <span class="grey-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <input type="text">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +31,16 @@ export default {
 </script>
 
 <style>
-
+.chat h2{
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.chat span{
+  font-size: 1.4em;
+}
+.chat .time{
+  display: block;
+  font-size: 1.2em;
+}
 </style>
 

730841e66a0106b7bc898aff8dd23692935c92e0
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-29 14:37:28 +0100

    lesson 85

--- geo-ninjas/src/components/layout/Navbar.vue
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -6,6 +6,7 @@
         <ul class="right">
           <li><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
           <li><a href="">Login</a></li>
+          <li><a @click="logout">Logout</a></li>
         </ul>
       </div>
     </nav>
@@ -13,11 +14,20 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
   name: 'Navbar',
   data(){
     return{
 
+    }
+  },
+  methods: {
+    logout(){
+      firebase.auth().signOut().then(() => {
+        this.$router.push({ name: 'Signup' })
+      })
     }
   }
 }

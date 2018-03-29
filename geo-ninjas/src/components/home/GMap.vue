a4b938bd65d6f909e90e58b66a90d9fffeb37cb8
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-29 16:03:40 +0100

    update

--- geo-ninjas/src/components/home/GMap.vue
+++ geo-ninjas/src/components/home/GMap.vue
@@ -7,7 +7,7 @@
 
 <script>
 export default {
-  name: 'Map',
+  name: 'GMap',
   data(){
     return{
 
--- geo-ninjas/src/router/index.js
+++ geo-ninjas/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Map from '@/components/home/Map'
+import GMap from '@/components/home/GMap'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Map',
-      component: Map
+      name: 'GMap',
+      component: GMap
     }
   ]
 })

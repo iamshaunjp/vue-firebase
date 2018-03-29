d822708954b2ae8ed26bc7a69e0661c4e2e14344
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-29 19:10:11 +0100

    lesson 89

--- geo-ninjas/src/components/home/GMap.vue
+++ geo-ninjas/src/components/home/GMap.vue
@@ -28,12 +28,30 @@ export default {
     }
   },
   mounted(){
+    // get current user
+    let user = firebase.auth().currentUser
+
     //get current location
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(pos => {
         this.lat = pos.coords.latitude
         this.lng = pos.coords.longitude
-        this.renderMap()
+
+        // find the user record and then update geocoords
+        db.collection('users').where('user_id', '==', user.uid).get()
+        .then(snapshot => {
+          snapshot.forEach((doc) => {
+            db.collection('users').doc(doc.id).update({
+              geolocation: {
+                lat: pos.coords.latitude,
+                lng: pos.coords.longitude
+              }
+            })
+          })
+        }).then(() => {
+          this.renderMap()
+        })
+
       }, (err) => {
         console.log(err) 
       }, { maximumAge: Infinity }) // cached location

d57e117575d69db78fd0e5216d140e36891e8df5
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-30 09:23:29 +0100

    lesson 92

--- geo-ninjas/src/components/home/GMap.vue
+++ geo-ninjas/src/components/home/GMap.vue
@@ -25,6 +25,25 @@ export default {
         minZoom: 3,
         streetViewControl: false
       });
+
+      db.collection('users').get().then(users => {
+        users.docs.forEach(doc => {
+          let data = doc.data()
+          if(data.geolocation){
+            let marker = new google.maps.Marker({
+              position: {
+                lat: data.geolocation.lat,
+                lng: data.geolocation.lng
+              },
+              map
+            })
+            // add click event to marker
+            marker.addListener('click', () => {
+              console.log(doc.id)
+            })
+          }
+        })
+      })
     }
   },
   mounted(){

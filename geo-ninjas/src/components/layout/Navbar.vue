e928a1baa6a31dbe7c842a90719a202cb95e41b8
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-30 08:58:12 +0100

    lesson 91

--- geo-ninjas/src/components/layout/Navbar.vue
+++ geo-ninjas/src/components/layout/Navbar.vue
@@ -4,9 +4,10 @@
       <div class="container">
         <router-link :to="{ name: 'GMap' }" class="brand-logo left">GeoNinjas!</router-link>
         <ul class="right">
-          <li><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
-          <li><router-link :to="{ name: 'Login' }">Login</router-link></li>
-          <li><a @click="logout">Logout</a></li>
+          <li v-if="!user"><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
+          <li v-if="!user"><router-link :to="{ name: 'Login' }">Login</router-link></li>
+          <li v-if="user"><a>{{ user.email }}</a></li>
+          <li v-if="user"><a @click="logout">Logout</a></li>
         </ul>
       </div>
     </nav>
@@ -20,8 +21,18 @@ export default {
   name: 'Navbar',
   data(){
     return{
-
+      user: null
     }
+  },
+  created(){
+    // let user = firebase.auth().currentUser
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user){
+        this.user = user
+      } else {
+        this.user = null
+      }
+    })    
   },
   methods: {
     logout(){

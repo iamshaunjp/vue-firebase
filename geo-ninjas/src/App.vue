1d61e622717d423e94ee1c9a021b386d2026e923
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-30 12:04:22 +0100

    lesson 97

--- geo-ninjas/src/App.vue
+++ geo-ninjas/src/App.vue
@@ -16,5 +16,7 @@ export default {
 </script>
 
 <style>
-
+body{
+  background: #eee;
+}
 </style>
--- geo-ninjas/src/components/profile/ViewProfile.vue
+++ geo-ninjas/src/components/profile/ViewProfile.vue
@@ -85,6 +85,17 @@ export default {
 </script>
 
 <style>
-
+.view-profile .card{
+  padding: 20px;
+  margin-top: 60px;
+}
+.view-profile h2{
+  font-size: 2em;
+  margin-top: 0;
+}
+.view-profile li{
+  padding: 10px;
+  border-bottom: 1px solid #eee
+}
 </style>
 

655e8495dc4d3bcb9d01affcaf59c9d0141689ca
Shaun <shaunjp@shaun-pelling.com>
2018-04-26 09:47:08 +0100

    Update init.js

--- ninja-smoothies/src/firebase/init.js
+++ ninja-smoothies/src/firebase/init.js
@@ -11,6 +11,7 @@ var config = {
   messagingSenderId: "979366054783"
 };
 const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
 
 // export firestore database
 export default firebaseApp.firestore()
